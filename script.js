a18831dca2ab8e53d4d55d997cbd71280b89587e
TheRookiexe <mishrxa36@gmail.com>
2025-09-22 23:45:14 +0530

    books obj and functions Working.

---
+++ script.js
@@ -0,0 +1,17 @@
+const library = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read){
+    const book =new Book(title, author, pages, read);
+    library.push(book);
+    console.log(library);
+}
+
+addBookToLibrary("test book", "test author", "223", "Not read");
